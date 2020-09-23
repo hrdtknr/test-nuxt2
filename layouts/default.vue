2c2be284c6710f29f5fd3d7293b44d639efa7349
rino_pk <rino@pk.com>
2020-09-23 18:34:08 +0900

    a

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <div v-if="loading" id="loading">
+      <v-progress-circular indeterminate />
+    </div>
+    <v-content>
+      <nuxt />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    loading: false,
+  }),
+  created() {
+    this.$nuxt.$on("toggleLoading", (loading) => {
+      this.loading = loading;
+    });
+  },
+};
+</script>
+
+<style lang="scss">
+#loading {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  z-index: 9999;
+  position: fixed;
+  background-color: rgba(#000, 0.5);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container class="text-center my-12">
+    <v-btn color="secondary" @click="submit">Submit</v-btn>
+  </v-container>
+</template>
+
+<script>
+export default {
+  methods: {
+    submit() {
+      this.toggleLoading(true);
+      setTimeout(() => {
+        this.toggleLoading(false);
+      }, 3000);
+    },
+    toggleLoading(loading) {
+      this.$nuxt.$emit("toggleLoading", loading);
+    },
+  },
+};
+</script>
